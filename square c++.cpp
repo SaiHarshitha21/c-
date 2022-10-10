ae74e10d618992b820ac59e3ea6df0ac73e30f3f
Sai Harshitha <113003178+SaiHarshitha21@users.noreply.github.com>
2022-10-10 13:28:05 +0530

    Add files via upload

---
+++ square c++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n,square,cube;
+	cout<<"enter number";
+	cin>>n;
+	square=n*n;
+	cube=n*n*n;
+	cout<<"the square of the number is"<<square;
+	cout<<"\nthe cube of the number is"<<cube;
+	return 0;
+}
+    
